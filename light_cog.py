82a2946997b309cccb1c3245af62d55297828c53
Matt C <mcompton2002@gmail.com>
2021-02-18 22:22:09 -0500

    oopsie forgot these

--- light_cog.py
+++ light_cog.py
@@ -2,6 +2,12 @@ import os
 
 from assistant_utils import speak, sanitize
 
+# This cog is a special case. 
+# I've set up shell scripts to contact my LIFX bulb that are run like:
+# $ light off
+# $ light blue
+# $ light on
+# and so on. I'm happy to share them if others are interested
 
 class light:
     def __init__(self):
