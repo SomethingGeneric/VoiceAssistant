4c4a854efc52a66cd95685c6872faebcbd8ca447
Matt C <mcompton2002@gmail.com>
2021-02-18 22:48:42 -0500

    debian examples & minor changes

--- assist.py
+++ assist.py
@@ -7,7 +7,7 @@ cogs = [hello(), light(), check(), apply(), reboot()]
 # in theory cogs could have other init args, but why bother?
 # ()'s just because Python syntax
 
-credentials_file = "google-creds.json"
+credentials_file = "google_creds.json"
 
 # Don't process recognized words unless it starts with:
 wake_word = "system"
@@ -30,12 +30,18 @@ while True:
     done = False
 
     with sr.Microphone() as source:
-        audio_text = r.listen(source)
-        cont = r.recognize_google_cloud(
-            audio_data=audio_text, credentials_json=open(credentials_file).read()
-        )
+        try:
+            audio_text = r.listen(source)
+            cont = r.recognize_google_cloud(
+                audio_data=audio_text, credentials_json=open(credentials_file).read()
+            )
+        except Exception as e:
+            speak("Google cloud error: " + str(e))
 
     try:
+
+        # Google apparently tries to capitalize sometimes??
+        cont = cont.lower()
 
         info("Got: " + cont)
 
@@ -48,12 +54,15 @@ while True:
                     if term in cont:
                         cog.run(cont)
                         done = True
+                        break
 
             # no matching cog:
             if not done:
+                # are we done here?
                 if "exit" in cont:
                     speak("Goodbye.")
                     sys.exit(0)
+                # use tuxi to see if google can help
                 else:
                     os.system('tuxi -r "' + cont + '" > temp')
                     speak(open("temp").read())
--- sys_cogs.py
+++ sys_cogs.py
@@ -8,7 +8,7 @@ import os
 
 class check:
     def __init__(self):
-        self.triggers = ["check updates"]
+        self.triggers = ["check update", "check updates"]
 
     def run(self, raw):
         speak("Checking updates.")
@@ -17,19 +17,48 @@ class check:
         os.remove("upds")
 
 
+# an example of check for debian based systems
+"""
+class check:
+    def __init__(self):
+        self.triggers = ["check updates"]
+
+    def run(self, raw):
+        speak("Checking updates.")
+        os.system("sudo apt update ; sudo apt list --upgradeable | wc -l > upds")
+        speak("There are " + open("upds").read() + " updates for the system.")
+        os.remove("upds")
+"""
+
+
 class apply:
     def __init__(self):
         self.triggers = ["host update"]
 
     def run(self, raw):
         speak("Applying updates")
+        speak("You may be prompted for a password")
         os.system("doas pacman -Syu --noconfirm")
         speak("All updates applied.")
 
 
+# An example of apply for debian systems
+"""
+class apply:
+    def __init__(self):
+        self.triggers = ["host update"]
+
+    def run(self, raw):
+        speak("Applying updates")
+        speak("You may be prompted for a password")
+        os.system("sudo apt full-upgrade -y")
+        speak("All updates applied.")
+"""
+
+
 class reboot:
     def __init__(self):
-        self.triggers("restart computer")
+        self.triggers = ["restart computer"]
 
     def run(self, raw):
         speak("Goodbye. System restarting.")
